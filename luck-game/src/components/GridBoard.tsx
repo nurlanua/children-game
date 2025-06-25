04caf369ee86defb82a34c4f28b8cebb7ea4ed70
Azat Nurlan Uulu <73550146+nurlanua@users.noreply.github.com>
2025-06-25 20:53:23 +0000

    Implemented the numbers

--- luck-game/src/components/GridBoard.tsx
+++ luck-game/src/components/GridBoard.tsx
@@ -1,22 +1,49 @@
 import React from 'react'
 import GridSquare from './GridSquare'
 
-// Represents a 10 x 18 grid of grid squares
+// Represents a 10 x 10 grid of grid squares
 
 export default function GridBoard() {
 
-  // generates an array of 18 rows, each containing 10 GridSquares.
+  // generates an array of 10 rows, each containing 10 GridSquares.
+    const rows = 10;
+    const cols = 10;
 
-    const grid: JSX.Element[][] = [];
-    for (let row = 0; row < 10; row ++) {
-        grid.push([])
-        for (let col = 0; col < 10; col ++) {
-            grid[row].push(<GridSquare key ={`${col}${row}`} color="0" />)
+    const numberGrid: number[][] = []
+    const randomInt = (min: number, max: number) =>
+        Math.floor(Math.random() * (max - min + 1)) + min;
+    
+    for (let row = 0; row < rows; row ++) {
+        const rowArray: number[] = [];
+        for (let col = 0; col < cols; col ++) {
+            rowArray.push(randomInt(0,4));
         }
+        numberGrid.push(rowArray);
     }
 
-  // The components generated in makeGrid are rendered in div.grid-board
+    const targetNumber = numberGrid[0][0];
+    const grid: JSX.Element[][] = [];
 
+    for (let row = 0; row < rows; row++){
+        grid.push([]);
+        for (let col = 0; col < cols; col++){
+            const currentNumber = numberGrid[row][col];
+
+            const isTarget = (row === 0 && col === 0) ||
+                (row === 1 && col === 0 && numberGrid[row][col] === targetNumber) || // bottom
+                (row === 0 && col === 1 && numberGrid[row][col] === targetNumber);   // right
+
+            const color = isTarget ? "1":"0";
+
+            grid[row].push(
+                <GridSquare
+                  key={`${col}${row}}`}
+                  color={color}
+                  number={currentNumber}
+                  />
+            );
+        }
+    }
     return (
         <div className='grid-board'>
             {grid}
--- luck-game/src/components/GridSquare.tsx
+++ luck-game/src/components/GridSquare.tsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 // Represents a grid square with a color
 
-export default function GridSquare(props: { color: any }) {
+export default function GridSquare(props: { color: any, number: number }) {
   const classes = `grid-square color-${props.color}`
-  return <div className={classes} />
+  return <div className={classes}>
+    {props.number}
+  </div>
 }
